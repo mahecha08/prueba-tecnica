4ce5974907ce41c5b643b061e98d4f20564bda15
Albert Mahecha <albertreyes2812@gmail.com>
2026-01-07 21:08:26 -0500

    se ajusta el service/api.service para la implementacion del API

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -3,8 +3,16 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  private urlApi = 'https://rickandmortyapi.com/api/character';
+  constructor(private http: HttpClient) {}
 
+  public getData(): Observable<any> {
+    return this.http.get<any>(this.urlApi);
+  }
+  public getDataId(id: number) {
+    return this.http.get(`https://rickandmortyapi.com/api/character/${id}`);
+  }
 }

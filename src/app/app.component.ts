5876a0a7c39626d991760f92771de8cd52cc8258
Albert Mahecha <albertreyes2812@gmail.com>
2026-01-07 21:20:22 -0500

    pruebas basicas. 26%

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'MAHECHA CARE COLA';
+  title = 'prueba';
 }
--- src/app/inicio/inicio.component.spec.ts
+++ src/app/inicio/inicio.component.spec.ts
@@ -17,7 +17,7 @@ describe('InicioComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
 });

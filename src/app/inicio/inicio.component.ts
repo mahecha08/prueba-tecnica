5700cbfc76305e3e6c838fb7c8855f4bbc172d76
Albert Mahecha <albertreyes2812@gmail.com>
2026-01-07 21:10:31 -0500

    se implementa logica en el inicio.componets y su html para visualizar la tabla con los datos traidos del API

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -4,9 +4,21 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.scss']
+  styleUrls: ['./inicio.component.scss'],
 })
 export class InicioComponent {
+  data: any[] = [];
 
-  
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.getPersonajes();
+  }
+
+  getPersonajes() {
+    this.apiService.getData().subscribe((response) => {
+      this.data = response.results;
+      console.log(this.data);
+    });
+  }
 }
